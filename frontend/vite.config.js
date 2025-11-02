3fab5399fe28288d2aa47bdb993344566f26f2d7
leonalbania2016 <leonalbania2016@users.noreply.github.com>
2025-11-02 04:14:25 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-})
+  server: {
+    port: 5173,
+    historyApiFallback: true, // enable local routing
+  },
+  build: {
+    outDir: "dist", // ensure output folder matches Render’s publish directory
+  },
+});
